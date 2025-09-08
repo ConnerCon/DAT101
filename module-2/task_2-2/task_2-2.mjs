20f4ad864a98cd82ddf54037da95cd1d3897d602
Conner <preyapon2303@hotmail.com>
2025-09-08 13:56:45 +0200

    task 2.2 work in progress

--- module-2/task_2-2/task_2-2.mjs
+++ module-2/task_2-2/task_2-2.mjs
@@ -4,12 +4,22 @@ import { printOut, newLine } from "../../common/script/utils.mjs";
 
 printOut("--- Part 1 ----------------------------------------------------------------------------------------------");
 /* Put your code below here!*/
-printOut("Replace this with you answer!");
+let regnstykke = 2+3*2-4*6;
+let regnstykke2 = 2+3*(2-4)*6;
+printOut("2 + 3 * 2 - 4 * 6 = " + regnstykke);
+printOut("2 + 3 * (2 - 4) * 6 = " + regnstykke2);
 printOut(newLine);
 
 printOut("--- Part 2 ----------------------------------------------------------------------------------------------");
 /* Put your code below here!*/
-printOut("Replace this with you answer!");
+let meter = 25;
+let centimeter = 34;
+let inch = 25.4;
+
+let tomilimeter = (meter * 1000 + centimeter * 10) //first make m and cm to mm
+let toinch = (tomilimeter / inch);
+
+printOut("25 meters and 34 centimeters = " + toinch.toFixed(2) + " inches");
 printOut(newLine);
 
 printOut("--- Part 3 ----------------------------------------------------------------------------------------------");
