f59386bf828453f7e698adb90db90e0dddd5bd7e
Conner <preyapon2303@hotmail.com>
2025-09-13 16:05:35 +0200

    example commit

--- module-2/task_2-2/task_2-2.mjs
+++ module-2/task_2-2/task_2-2.mjs
@@ -16,7 +16,7 @@ let meter = 25;
 let centimeter = 34;
 let inch = 25.4;
 
-let tomilimeter = (meter * 1000 + centimeter * 10) //first make m and cm to mm
+let tomilimeter = (meter * 1000 + centimeter * 10); //first make m and cm to mm
 let toinch = (tomilimeter / inch);
 
 printOut("25 meters and 34 centimeters = " + toinch.toFixed(2) + " inches");
@@ -24,11 +24,21 @@ printOut(newLine);
 
 printOut("--- Part 3 ----------------------------------------------------------------------------------------------");
 /* Put your code below here!*/
-printOut("Replace this with you answer!");
+let days = 3;
+let hours = 12;
+let minutes = 14;
+let seconds = 45;
+
+let tominutes = (days * 1440 + (hours * 60) + minutes + seconds / 60); //convert everthing to minutes and sums it
+
+printOut("3 days, 12 hours, 14 minutes, and 45 seconds = " + tominutes + " minutes");
 printOut(newLine);
 
 printOut("--- Part 4 ----------------------------------------------------------------------------------------------");
 /* Put your code below here!*/
+let 
+let totalminutes = 6322.52;
+
 printOut("Replace this with you answer!");
 printOut(newLine);
 
