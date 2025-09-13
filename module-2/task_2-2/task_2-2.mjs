3cd7ac524a176e31b86b008503398afef3503ce2
Conner <preyapon2303@hotmail.com>
2025-09-14 00:57:08 +0200

    test again

--- module-2/task_2-2/task_2-2.mjs
+++ module-2/task_2-2/task_2-2.mjs
@@ -64,14 +64,14 @@ printOut(newLine);
 
 printOut("--- Part 6 ----------------------------------------------------------------------------------------------");
 /* Put your code below here!*/
-let quote = "There is much between heaven and earth that we do not understand.";
+let text = "There is much between heaven and earth that we do not understand.";
 
 let length = Text.length;
 let character19 = text[19];
 let part = Text.slice(35,43);
 let index = text.indexOf("earth");
 
-printOut(quote);
+printOut(text);
 printOut("This text has " + length + " characters.");
 printOut("The character at position 19 is: " + character19);
 printOut("The substring of 35 and 8 places is: " + part);
