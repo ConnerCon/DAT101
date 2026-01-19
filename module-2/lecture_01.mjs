843d46fe8e406d216a4a5a2a1f06333ae94f9438
Conner <preyapon2303@hotmail.com>
2026-01-19 08:29:46 +0100

    test commit

--- module-2/lecture_01.mjs
+++ module-2/lecture_01.mjs
@@ -4,5 +4,5 @@ import {printOut} from "../../common/script/utils.mjs";
 
 //Create a function that print hello World to console
 function printHelloWorld() {
-    printOut("Hello World");
+    printOut("Hello World!");
 }
